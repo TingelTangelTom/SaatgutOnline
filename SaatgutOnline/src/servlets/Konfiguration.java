182ad05be83e97590e4b6e53f4ead83c456b9e8c
Rainer Olbricht <billigladen@gmx.de>
2014-04-29 11:24:27 +0200

    ServletContext() eingebunden: servlets.Konfiguration und web.xml

---
+++ SaatgutOnline/src/servlets/Konfiguration.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class Konfiguration
+ */
+@WebServlet("/Konfiguration")
+public class Konfiguration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Konfiguration() {
+        super();
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig konfiguration) throws ServletException {
+		
+		super.init(konfiguration);
+		getServletContext().setAttribute("dbHost", konfiguration.getInitParameter("dbHost"));
+		getServletContext().setAttribute("dbPort", konfiguration.getInitParameter("dbPort"));
+		getServletContext().setAttribute("datenbank", konfiguration.getInitParameter("datenbank"));
+		getServletContext().setAttribute("dbBenutzer", konfiguration.getInitParameter("dbBenutzer"));
+		getServletContext().setAttribute("dbPasswort", konfiguration.getInitParameter("dbPasswort"));
+	}
+}
