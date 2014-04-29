024fa6a90fcbdc7602d86fcbe2e93972c9c737ee
TingelTangelTom <tom.javaee@swbmail.de>
2014-04-29 16:06:18 +0200

    GET- und POST-Parameter werden nun getrennt angezeigt, gefolgt von den Session-Inhalten

--- SaatgutOnline/src/servlets/FussbereichServlet.java
+++ SaatgutOnline/src/servlets/FussbereichServlet.java
@@ -33,32 +33,6 @@ public class FussbereichServlet extends HttpServlet
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		
-		// DEBUG-Ausgabe für request-Parameter
-		//TODO remove
-		{
-		System.out.println("\n---(POST/GET) request-Parameter---");		
-		Enumeration<String> paramaters = request.getParameterNames();
-		while (paramaters.hasMoreElements())
-		{
-			String name = paramaters.nextElement();
-			String value = request.getParameter(name);
-			System.out.println(name + " = " + value);			
-		}		
-		System.out.println("----------------------------------");
-		}		
-		{
-		System.out.println("\n---Session-Inhalte---");
-		Enumeration<String> sessionParamaters = request.getSession().getAttributeNames();
-		while (sessionParamaters.hasMoreElements())
-		{
-			String name = sessionParamaters.nextElement();
-			System.out.println(name + " = " + request.getSession().getAttribute(name));			
-		}
-		System.out.println("---------------------");
-		}
-		
-		
 		// Fussbereichview schliessen
 		FussbereichView fussbereichView = new FussbereichView(response);
 		
@@ -71,8 +45,27 @@ public class FussbereichServlet extends HttpServlet
 		// Fussbereich schliessen
 		fussbereichView.outFussbereichEnde();
 		
-
-		
+		// DEBUG-Ausgabe für request-Parameter (GET)
+		//TODO remove
+		{
+		System.out.println("\n----(GET) request-Parameter----");		
+		Enumeration<String> paramaters = request.getParameterNames();
+		while (paramaters.hasMoreElements())
+		{
+			String name = paramaters.nextElement();
+			String value = request.getParameter(name);
+			System.out.println(name + " = " + value);			
+		}		
+		System.out.println("----------------------------------");
+		}	
+		System.out.println("\n---Session-Inhalte---");
+		Enumeration<String> sessionParamaters = request.getSession().getAttributeNames();
+		while (sessionParamaters.hasMoreElements())
+		{
+			String name = sessionParamaters.nextElement();
+			System.out.println(name + " = " + request.getSession().getAttribute(name));			
+		}
+		System.out.println("---------------------");
 		
 		
 	}
@@ -83,8 +76,43 @@ public class FussbereichServlet extends HttpServlet
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
 			IOException
-	{		
-		doGet(request, response);
+	{	
+		// Fussbereichview schliessen
+		FussbereichView fussbereichView = new FussbereichView(response);
+		
+		// Fussbereich oeffnen
+		fussbereichView.outFussbereichAnfang();
+		
+		// Inhalte des Fussbereiches ausgeben
+		fussbereichView.outFussbereichInhalt();
+		
+		// Fussbereich schliessen
+		fussbereichView.outFussbereichEnde();
+		
+		
+		
+		// DEBUG-Ausgabe für request-Parameter (POST)
+		//TODO remove
+		{
+		System.out.println("\n---(POST)  request-Parameter---");		
+		Enumeration<String> paramaters = request.getParameterNames();
+		while (paramaters.hasMoreElements())
+		{
+			String name = paramaters.nextElement();
+			String value = request.getParameter(name);
+			System.out.println(name + " = " + value);			
+		}		
+		System.out.println("----------------------------------");
+		}		
+		System.out.println("\n---Session-Inhalte---");
+		Enumeration<String> sessionParamaters = request.getSession().getAttributeNames();
+		while (sessionParamaters.hasMoreElements())
+		{
+			String name = sessionParamaters.nextElement();
+			System.out.println(name + " = " + request.getSession().getAttribute(name));			
+		}
+		System.out.println("---------------------");
+	
+		
 	}
-
 }
